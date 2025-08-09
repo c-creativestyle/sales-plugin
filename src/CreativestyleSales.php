437c5bc98dc83b88a252c2cea2ed8ff99bad1cc8
c-creativestyle <krzysztof.26e+cs@gmail.com>
2025-08-09 10:36:32 +0000

    CS-1 Sales plugin

---
+++ src/CreativestyleSales.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Free to use, modify, distribute. No warranty. Include license.
+ */
+
+declare(strict_types=1);
+
+namespace Creativestyle\Sales;
+
+use Shopware\Core\Framework\Plugin;
+
+class CreativestyleSales extends Plugin
+{
+}
